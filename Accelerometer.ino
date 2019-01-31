5f76f829944ce97b8e65e2ea6f74129b3d1be4e4
LoganBrist <44846549+LoganBrist@users.noreply.github.com>
2019-01-31 15:26:16 -0700

    Add files via upload

---
+++ Accelerometer.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  MMA8452Q::read();
+  int x_val = x;
+  int y_val = y;
+  int z_val = z;
+
+}

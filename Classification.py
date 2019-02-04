f76a0b605458f862c66979a17999a6107889622a
LoganBrist <44846549+LoganBrist@users.noreply.github.com>
2019-02-03 23:35:52 -0700

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -12,34 +12,69 @@ Created on Thu Jan 31 18:07:21 2019
 
 #Import support vector machine functions
 from sklearn import svm
+import os
+from matplotlib import pyplot as plt
 
-#import text file with time/acceleration stamps
-data = open('exampledata.txt','r') 
 
-#separate data into two lists
-t = []    #time vector
-a = []    #acceleration vector
-y = []    #classification vector
+#separateing the data 
+x = []    
+y = []
+z = []
+classification = []
 
-t_temp = []   #vector for current file
-a_temp = []   #vector for current file
-for line in data:  
-    word = line.split()
+#Retrieve data for every trial
+file_names = os.listdir()
+
+for file in file_names:
+    if 'data' and '.txt' in file:
+        pass #execute code to read data
+    else: 
+        continue #check next file
+        
+    data = open(file,'r') 
     
-    if line == 0:  
-        y.append(word[1])   #issue here, but gtg
-    if  line == 1:
-        continue
-    else:
-        t_temp.append(float(word[0]))
-        a_temp.append(float(word[1]))
+    #vectors for current file
+    x_temp = []  
+    y_temp = []   
+    z_temp = []
     
-#add data to one row of the entire data set    
-t.append(t_temp)
-a.append(a_temp)
+    for line in data:
+        #separate characters by spaces
+        word = line.split()
+        
+        #if the row contains numbers, separate them into the vectors
+        try:
+            x_temp.append(float(word[0]))
+            y_temp.append(float(word[1]))
+            z_temp.append(float(word[2]))
 
-print("time:", t)
-print("acc.:", a)
-print("classification:",y)
+        except:
+            pass
+        
+        #if the row is the classification label, record the classification
+        try:
+            if word[0] == 'classification:':
+                classification.append(word[1])
+        
+        except:
+            pass
+        
+    #add data to one row of the entire data set    
+    x.append(x_temp)
+    y.append(y_temp)
+    z.append(z_temp)
+    
+    data.close()
+
+
+# Plots the data for each trial
+for i in range (0,len(x)):
+    plt.plot(x[i],label="X")
+    plt.plot(y[i],label="Y")
+    plt.plot(z[i],label="Z")
+    plt.legend()
+    plt.title('Acceleration Data (' + classification[i] + ')')
+    plt.figure()
+
 
 # tbc.......

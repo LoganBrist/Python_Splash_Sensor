f238537b8655fe68ed9d49947bceb9855588b5f6
LoganBrist <44846549+LoganBrist@users.noreply.github.com>
2019-02-03 23:35:07 -0700

    Update Accelerometer.ino

--- Accelerometer.ino
+++ Accelerometer.ino
@@ -1,13 +1,78 @@
-void setup() {
-  // put your setup code here, to run once:
+// SparkFun Code
 
+#include <Wire.h> // Library for I2C
+#include <SparkFun_MMA8452Q.h> //SFE_MMA8452Q library
+
+// Instance of the MMA8452Q class.
+MMA8452Q accel;
+
+// Starts serial and initializes the
+//  accelerometer.
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("X        Y        Z");
+
+  accel.init();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  MMA8452Q::read();
-  int x_val = x;
-  int y_val = y;
-  int z_val = z;
+// Check for new data from the accelerometer and print it out if it's available.
+void loop()
+{
+  if (accel.available())
+  {
+    accel.read();
+    printCalculatedAccels();
+    //printOrientation();
+    
+    Serial.println();
+  }
+  delay(250); //Log data every .25 seconds
+}
 
+// Using the accel.x, accel.y and accel.z variables. Before using these variables you must call accel.read().
+void printAccels()
+{
+  Serial.print(accel.x, 3);
+  Serial.print("\t");
+  Serial.print(accel.y, 3);
+  Serial.print("\t");
+  Serial.print(accel.z, 3);
+  Serial.print("\t");
+}
+
+// Using the accel.cx, accel.cy, and accel.cz variables.
+void printCalculatedAccels()
+{ 
+  Serial.print(accel.cx, 3);
+  Serial.print("\t");
+  Serial.print(accel.cy, 3);
+  Serial.print("\t");
+  Serial.print(accel.cz, 3);
+  Serial.print("\t");
+}
+
+
+// Using the accel.readPL() function, which reads the portrait/landscape status of the sensor.
+void printOrientation()
+{
+  byte pl = accel.readPL();
+  switch (pl)
+  {
+  case PORTRAIT_U:
+    Serial.print("Portrait Up");
+    break;
+  case PORTRAIT_D:
+    Serial.print("Portrait Down");
+    break;
+  case LANDSCAPE_R:
+    Serial.print("Landscape Right");
+    break;
+  case LANDSCAPE_L:
+    Serial.print("Landscape Left");
+    break;
+  case LOCKOUT:
+    Serial.print("Flat");
+    break;
+  }
 }

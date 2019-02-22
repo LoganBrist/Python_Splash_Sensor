f6280aea20dbde91dc5ce4b51086def3e6c2ff28
LoganBrist <44846549+LoganBrist@users.noreply.github.com>
2019-02-22 01:35:13 -0700

    Update Accelerometer_SD.ino

--- Accelerometer_SD.ino
+++ Accelerometer_SD.ino
@@ -1,5 +1,9 @@
 // SparkFun Code
 
+---------------------------------------------------------------------------------------
+//   Libraries and global variables
+
+
 #include <SparkFun_MMA8452Q.h> //SFE_MMA8452Q library
 #include <SPI.h> // Library for SPI (communication with SD)
 #include <SD.h>  // SD card functions
@@ -13,7 +17,78 @@ File dataFile;
 const uint8_t BUTTON         = 3;
 const uint8_t PULL_HIGH      = 2;
 const uint8_t GREEN_LED      = 5;
+     uint64_t Blink_Counter  = 0; 
+     
+---------------------------------------------------------------------------------------
+//   Functions
 
+///////////////////////////////////////////////////////////////////////////////////////
+// Initilize SD card function
+///////////////////////////////////////////////////////////////////////////////////////
+void start_SD(){
+    bool SD_success = SD.begin(4);
+    if (SD_success)
+        Serial.println("SD card initialized successful...");
+    else {
+        Serial.println("SD card not recognized... Insert card and then hit resert button (program stopping)");
+        while(1){
+          //blink light
+          Blink_Counter++;
+          bool state = digitalRead(GREEN_LED);
+          if (Blink_Counter % 10000 == 0)
+              state = !state;
+              
+          digitalWrite(GREEN_LED,state);
+            if (Button_Pushed (BUTTON, 50)) {
+              LED_TOGGLE(GREEN_LED,LOW);
+              start_SD();
+              break;
+              }
+            }
+        }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//Button_Pushed Function - Returns true if the button is held for the time interval
+/////////////////////////////////////////////////////////////////////////////////////
+
+bool Button_Pushed (int pin_number, int interval){
+    elapsedMillis timeElapsed;
+    while (digitalRead(pin_number) == 1) {
+      if (timeElapsed > interval){
+        while(digitalRead(pin_number) == 1) {}
+        return true;
+      }  
+    }
+    return false;
+  }
+/////////////////////////////////////////////////////////////////////////////////////
+//LED_State Function- Flash LED when changed
+/////////////////////////////////////////////////////////////////////////////////////
+void LED_TOGGLE (int led_name, bool EndState) {
+    //Initial state, to determine ending state and blink pattern
+    //digitalRead(led_name);
+
+    int n = 0;
+    //Blinks
+    while(n < 4){  
+      digitalWrite(led_name, EndState);
+      delay(100);
+      digitalWrite(led_name, !EndState);
+      delay(100);
+      n++;
+    }
+    
+    //Toggle value
+    digitalWrite(led_name, EndState);
+}
+
+
+-------------------------------------------------------------------------------------
+// MAIN
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Setup function
 /////////////////////////////////////////////////////////////////////////////////////
 void setup(){
   //Pin Assignments
@@ -24,50 +99,16 @@ void setup(){
   //Pull_High Pin
   digitalWrite(PULL_HIGH, HIGH);
   digitalWrite(GREEN_LED, LOW);
-  //Intitialze SD writing and accelerometer
+  
+  //Intitialze serial communication
   Serial.begin(9600);
-  SD.begin(4);
+  Serial.println("Serial connection initialized successfully...");
+
+  //Initialize Accelerometer
   accel.init();
-}
-/////////////////////////////////////////////////////////////////////////////////////
-//Button_Pushed Function - Returns true if the button is held for the time interval
-/////////////////////////////////////////////////////////////////////////////////////
 
-bool Button_Pushed (int pin_number, int interval){
-    elapsedMillis timeElapsed;
-    while (digitalRead(pin_number) == 1) {
-      if (timeElapsed > interval){
-        Serial.println("Button Pressed");
-        LED_TOGGLE(GREEN_LED);
-        while(digitalRead(pin_number) == 1) {}
-        Serial.println("Button Released");
-        return true;
-      }  
-    }
-    return false;
-  }
-/////////////////////////////////////////////////////////////////////////////////////
-//LED_State Function- Flash LED when changed
-/////////////////////////////////////////////////////////////////////////////////////
-void LED_TOGGLE (int led_name) {
-    //Initial state, to determine ending state and blink pattern
-    int State = digitalRead(led_name);
- 
-    Serial.println(State);
-    int n = 0;
-    //Blinks
-    while(n < 4){  
-      digitalWrite(led_name, !State);
-      delay(100);
-      digitalWrite(led_name, State);
-      delay(100);
-      n++;
-    }
-    
-    //Toggle value
-    digitalWrite(led_name, !State);
+  Serial.println("Ready to log data.");
 }
-
 /////////////////////////////////////////////////////////////////////////////////////
 // Data Collection loop
 /////////////////////////////////////////////////////////////////////////////////////
@@ -77,35 +118,41 @@ void loop(){
   while(1){
   bool Code_Is_Running = false;
   
-  //Strings for filename and test number
+   //Strings for filename and test number
   String Test_no;
   String filename;
 
-  //Check if file exists, incrementing number extension each time
+  // Hold program until button press
+  while(!Code_Is_Running) {  
+    if (Button_Pushed(BUTTON, 50)){
+      LED_TOGGLE(GREEN_LED,HIGH);
+      Code_Is_Running = true;
+    }
+   }
+
+  //Initialize SD
+  start_SD();
+  digitalWrite(GREEN_LED,HIGH); //Button toggle signal is put here to overwrite the blink
+  
+  //Check for an open filename
   while(true){
       Test_no  = String(test_no);
       filename = "data" + Test_no + ".txt"; 
       if (SD.exists(filename) == false)
           break;
       test_no++;
-  }
-
+    }
+  
   //Open file
   Serial.println(filename + " opened");
   dataFile = SD.open(filename, FILE_WRITE); 
+  Serial.println("Starting data logging");
+  dataFile.println("X        Y        Z");
     
-  // Check for button hold to start recording
-  while(!Code_Is_Running) {  
-    if (Button_Pushed(BUTTON, 50)){
-      Code_Is_Running = true;
-      Serial.println("Starting data logging");
-      dataFile.println("X        Y        Z");
-    }
-    }
-      
-  // Log Data
+     
+  // Log Data loop
   while(Code_Is_Running) {     
-  // Check for new data from the accelerometer and print it out if it's available.
+    //Write accelerometer data to SD card
     if (accel.available()){
       accel.read();
       dataFile.print(accel.cx, 3);
@@ -114,19 +161,18 @@ void loop(){
       dataFile.print("\t");
       dataFile.print(accel.cz, 3);
       dataFile.print("\t");
-      
       dataFile.println();    
       } 
 
     // Check for button hold to stop recording
     if (Button_Pushed(BUTTON, 50)){
+      LED_TOGGLE(GREEN_LED,LOW);
       dataFile.close();
       Serial.println(filename + " closed");
       Serial.println();
       Code_Is_Running = false;     
-    }
+      } 
     }
     test_no += 1;
   }   
 }
-
